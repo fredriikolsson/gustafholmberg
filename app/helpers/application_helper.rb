5b087f2b42ce8f8d02193ec44ebc120069cb1dda
Fredrik Olsson <fredrikolsson@MacBook-Pro-som-tillhor-Fredrik.local>
2022-10-01 18:30:15 +0200

    Add viewport for mobile devices
    Should fix the small site on mobile devices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def viewport_meta_tag
+    tag.meta(name: 'viewport', content: 'width=device-width, initial-scale=1')
+  end
+
   def show_svg(path)
     raw File.read("app/assets/images/#{path}") # rubocop:disable Rails/OutputSafety
   end
