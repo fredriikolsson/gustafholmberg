b913d1309a56d57430d9df4c7fef27375862c69d
Fredrik Olsson <fredrikolsson@MacBook-Pro-som-tillhor-Fredrik.local>
2023-01-19 21:36:01 +0100

    Add sign in and sign out capabilites

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,21 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_post, only: %i[show edit update destroy]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show; end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit; end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -47,7 +43,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
@@ -59,12 +54,10 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :body)
   end
--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -2,7 +2,7 @@
 
 module NavbarHelper
   def navbar_link(text, url, options = {})
-    options[:class] = 'mx-5 hover:text-gray-900'
+    options[:class] = "#{options[:class]} mx-5 hover:text-gray-900"
     link_to text, url, **options
   end
 end
